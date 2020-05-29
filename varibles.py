4b112b3ae417d5f002cb1dba68018f0c50efed20
Neethu Sara Thomas <neethusarathomas@gmail.com>
2020-05-30 01:03:44 +0530

    adding varible.py

---
+++ varibles.py
@@ -0,0 +1,15 @@
+price = 10
+price = 20
+print(price)
+rating = 4.9
+name = 'Neethu'
+is_published = True
+print(name)
+print(rating)
+print(is_published)
+
+# First Task
+
+name='John Smith'
+age=20
+is_newpatient=True
